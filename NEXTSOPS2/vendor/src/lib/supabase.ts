5af0fd7cfdac8a3ced31580889f404e0b4e15e42
Owenoz <muyanjaowen3@gmail.com>
2026-05-04 16:10:50 +0300

    Clean repository + proper .gitignore

--- NEXTSOPS2/vendor/src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vydvmmalqldpjvrtadbd.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5ZHZtbWFscWxkcGp2cnRhZGJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NTI0NjQsImV4cCI6MjA5MzQyODQ2NH0.5YVJeUVT2Ufb57u_K6Uu_VESa3h_jPZIIQjMcO3uICs'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
